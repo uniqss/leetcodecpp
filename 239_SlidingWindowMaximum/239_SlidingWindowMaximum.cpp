c375075330033535ec53e171208821052ae077a9
uniqss <uniqs@163.com>
2021-12-17 00:18:45 +0800

    monoqueue is useful

--- 239_SlidingWindowMaximum/239_SlidingWindowMaximum.cpp
+++ 239_SlidingWindowMaximum/239_SlidingWindowMaximum.cpp
@@ -1,12 +1,13 @@
 #include "../stl.h"
 
+// force
 class Solution {
    public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> maxSlidingWindow(const vector<int>& nums, int k) {
     }
 };
 
-void test(vector<int>&& nums, int k, const vector<int>& expect) {
+void test(const vector<int>& nums, int k, const vector<int>& expect) {
     pvi(nums);
     pi(k);
     Solution s;
@@ -22,6 +23,7 @@ void test(vector<int>&& nums, int k, const vector<int>& expect) {
 }
 
 int main() {
+    test({7, 2, 4}, 2, {7, 4});
     test({1, 3, -1, -3, 5, 3, 6, 7}, 3, {3, 3, 5, 5, 6, 7});
     test({1}, 1, {1});
     return 0;
---
+++ 239_SlidingWindowMaximum/239_SlidingWindowMaximum2.cpp
@@ -0,0 +1,66 @@
+#include "../stl.h"
+
+// nomoqueue
+class MonoQueue {
+    // first: val
+    // second: the numbers of elements deleted before
+    deque<pair<int, int>> vals;
+
+   public:
+    void push(int val) {
+        int count = 0;
+        while (!vals.empty() && val > vals.back().first) {
+            count += vals.back().second + 1;
+            vals.pop_back();
+        }
+        vals.push_back({val, count});
+    }
+    void pop() {
+        if (vals.front().second > 0) {
+            --vals.front().second;
+        } else {
+            vals.pop_front();
+        }
+    }
+    int max() {
+        return vals.front().first;
+    }
+};
+
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(const vector<int>& nums, int k) {
+        MonoQueue mq;
+        vector<int> ret;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            mq.push(nums[i]);
+            if ((int)i >= k - 1) {
+                ret.push_back(mq.max());
+                mq.pop();
+            }
+        }
+        return ret;
+    }
+};
+
+void test(const vector<int>& nums, int k, const vector<int>& expect) {
+    pvi(nums);
+    pi(k);
+    Solution s;
+    auto ret = s.maxSlidingWindow(nums, k);
+    if (ret == expect) {
+        ps("ok");
+    } else {
+        ps("##### not ok");
+        pvi(expect);
+        pvi(ret);
+    }
+    pnewline();
+}
+
+int main() {
+    test({7, 2, 4}, 2, {7, 4});
+    test({1, 3, -1, -3, 5, 3, 6, 7}, 3, {3, 3, 5, 5, 6, 7});
+    test({1}, 1, {1});
+    return 0;
+}

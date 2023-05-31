4126ad182422bb2524ccc528832ee31195e5d849
uniqs <uniqs@163.com>
2023-05-31 16:09:52 +0000

    一切的递归回溯动规在自底向上的c面前都是纸老虎

--- 0290/290_WordPattern.cpp
+++ 0290/290_WordPattern.cpp
@@ -2,23 +2,7 @@
 
 class Solution {
    public:
-    bool wordPattern(string pattern, string s) {
-        vector<int> starts = {0};
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ' ') starts.push_back(i + 1);
-        }
-        if (starts.size() != pattern.size()) return false;
-        unordered_map<char, vector<int>> ptposes;
-        for (int i = 0; i < pattern.size(); i++) {
-            ptposes[pattern[i]].push_back(i);
-        }
-        for (auto it : ptposes) {
-            if (it.second.size() == 1) continue;
-            for (int i = 1; i < it.second.size(); i++) {
-                
-            }
-        }
-    }
+    bool wordPattern(string pattern, string s) {}
 };
 
 void test(string pattern, string s, bool expect) {
@@ -36,6 +20,7 @@ void test(string pattern, string s, bool expect) {
 }
 
 int main() {
+    test("abba", "dog dog dog dog", false);  // 这里还要check互相不相等。。。
     test("abba", "dog cat cat dog", true);
     test("abba", "dog cat cat fish", false);
     test("aaaa", "dog cat cat dog", false);
---
+++ 0290/290_WordPattern1.cpp
@@ -0,0 +1,75 @@
+#include "../inc.h"
+
+class Solution {
+   public:
+    bool wordPattern(string pattern, string s) {
+        vector<int> starts = {0};
+        auto ssize = s.size();
+        s.push_back(' ');
+        for (int i = 0; i < ssize; i++) {
+            if (s[i] == ' ') starts.push_back(i + 1);
+        }
+        if (starts.size() != pattern.size()) return false;
+        vector<int> ptposes[26];
+        for (int i = 0; i < pattern.size(); i++) {
+            ptposes[pattern[i] - 'a'].push_back(i);
+        }
+        for (auto& it : ptposes) {
+            if (it.size() <= 1) continue;
+            auto pos0 = it[0];
+            for (int i = 1; i < it.size(); i++) {
+                auto start0 = starts[pos0];
+                auto start = starts[it[i]];
+                while (start < ssize && start0 < ssize && s[start] != ' ' && s[start0] != ' ') {
+                    if (s[start] != s[start0]) return false;
+                    ++start;
+                    ++start0;
+                }
+                if (s[start] != ' ' || s[start0] != ' ') return false;
+            }
+        }
+        for (int i = 0; i < 26 - 1; i++) {
+            if (ptposes[i].empty()) continue;
+            for (int j = i + 1; j < 26; j++) {
+                if (ptposes[j].empty()) continue;
+                auto start1 = starts[ptposes[i][0]];
+                auto start2 = starts[ptposes[j][0]];
+                bool equal = true;
+                while (start1 < ssize && start2 < ssize && s[start1] != ' ' && s[start2] != ' ') {
+                    if (s[start1] != s[start2]) {
+                        equal = false;
+                        break;
+                    }
+                    ++start1, ++start2;
+                }
+                if (equal && (s[start1] != ' ' || s[start2] != ' ')) equal = false;
+                if (equal) return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+void test(string pattern, string s, bool expect) {
+    Solution so;
+    auto ret = so.wordPattern(pattern, s);
+    if (ret == expect) {
+        print("ok");
+    } else {
+        print("not ok");
+        print(pattern);
+        print(s);
+        print(expect);
+        print(ret);
+    }
+}
+
+int main() {
+    test("abba", "dog dog dog dog", false);  // 这里还要check互相不相等。。。
+    test("abba", "dog cat cat dog", true);
+    test("abba", "dog cat cat fish", false);
+    test("aaaa", "dog cat cat dog", false);
+
+    return 0;
+}
